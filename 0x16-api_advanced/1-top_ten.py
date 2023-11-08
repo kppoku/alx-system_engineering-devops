06e1647d0defb83f65fcc91fc4daabb7eadac4e6
KP <kppoku@gmail.com>
2023-11-08 06:08:56 +0000

    api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""function that queries the reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit."""
-
-
+"""
+queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """function that queries the reddit API and prints the
-        titles of the first 10 hot posts listed for
-        a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
+    """prints titles of the first 10hot posts"""
+    headers = {
+        "User-Agent": "0x16. API_advanced-e_kiminza"
+    }
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
         print("None")
-
+        return
+    response_ = response.json().get("data")
+    for child in response_.get("children"):
+        print(child.get("data").get("title"))
